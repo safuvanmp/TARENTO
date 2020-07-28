2c6f1777d7514b4de45b6f88b2103d41be3344f6
Safuvan MP <47299582+safuvanmp@users.noreply.github.com>
2020-07-28 18:00:47 +0530

    Add files via upload

---
+++ add.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int a,b,result=0;
+std::cout<<"ENTER YOUR FIRST NUMBER"<<std::endl;
+std::cin>>a;
+std::cout<<"ENTER YOUR SECOND NUMBER"<<std::endl;
+std::cin>>b;
+result=a+b;
+std::cout<<"RESULT = "<<result<<std::endl;
+}
